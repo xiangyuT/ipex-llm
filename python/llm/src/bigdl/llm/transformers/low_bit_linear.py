b091d6596fe2caa7956409f70dca9a50fcdb5cd2
Yang Wang <yang3.wang@intel.com>
2024-01-31 14:45:04 -0800

    change to llm.cpp

--- python/llm/src/bigdl/llm/transformers/low_bit_linear.py
+++ python/llm/src/bigdl/llm/transformers/low_bit_linear.py
@@ -562,7 +562,7 @@ class LowBitLinear(nn.Linear):
                     result = result.to(x.dtype)
                 else:
                     x_2d = x_2d.half()
-                    result = torch.ops.torch_ipex.mm_int4(x_2d, self.qweight, self.scales, self.zeros, 64)
+                    result = linear_q4_0.mm_int4(x_2d, self.qweight, self.scales, self.zeros)
                     # result = linear_q4_0.forward_new(x_2d, self.weight.data, self.weight.qtype,
                     #                                  input_seq_size)
             result = result.view(new_shape)
